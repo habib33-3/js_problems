e53e62d8c02729827e2342630c56769e46e1a097
Habibur Rahman <habib.swi16@gmail.com>
2023-08-02 20:33:08 +0600

    mile2km

---
+++ inchToFeet.js
@@ -0,0 +1,8 @@
+function inch2feet(inch) {
+  const feet = inch / 12;
+  console.log(feet);
+}
+
+let inch = 1444;
+
+inch2feet(inch);
---
+++ mileToKm.js
@@ -0,0 +1,9 @@
+function mile2Km(mile) {
+  let km = mile * 1.67;
+
+  console.log(km);
+}
+
+let mile = 45;
+
+mile2Km(mile);
